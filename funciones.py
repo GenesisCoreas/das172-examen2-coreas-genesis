cfdeb7db401f859394932e9e61f13be4e2199df4
Genesis Coreas <CR220260@alumno.udb.edu.sv>
2026-09-08 11:28:06 -0600

    feat(funciones): implement scalar multiplication

--- funciones.py
+++ funciones.py
@@ -307,3 +307,16 @@ def sumar_matrices(A, B):
 
     return resultado
 
+def multiplicar_por_escalar(matriz, escalar):
+    resultado = []
+
+    for fila in matriz:
+        nueva_fila = []
+
+        for elemento in fila:
+            nueva_fila.append(elemento * escalar)
+
+        resultado.append(nueva_fila)
+
+    return resultado
+
