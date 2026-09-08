bb874c8732ecac96180ee85c96560c462e9691d6
Genesis Coreas <CR220260@alumno.udb.edu.sv>
2026-09-08 11:35:40 -0600

    feat(funciones): implement main diagonal sum calculation

--- funciones.py
+++ funciones.py
@@ -372,3 +372,11 @@ def es_matriz_diagonal(matriz):
 
     return True
 
+def sumar_diagonal_principal(matriz):
+    suma = 0
+
+    for i in range(len(matriz)):
+        suma += matriz[i][i]
+
+    return suma
+
