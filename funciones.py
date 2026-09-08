2155234bf34ad13157489a06c3f010a0fbde60cd
Genesis Coreas <CR220260@alumno.udb.edu.sv>
2026-09-08 10:37:43 -0600

    feat(funciones): implement matrix comparison

--- funciones.py
+++ funciones.py
@@ -96,3 +96,17 @@ def transformar_matriz(matriz):
 
     return transformada
 
+def comparar_matrices(matriz1, matriz2):
+    if len(matriz1) != len(matriz2):
+        return False
+
+    for i in range(len(matriz1)):
+        if len(matriz1[i]) != len(matriz2[i]):
+            return False
+
+        for j in range(len(matriz1[i])):
+            if matriz1[i][j] != matriz2[i][j]:
+                return False
+
+    return True
+
